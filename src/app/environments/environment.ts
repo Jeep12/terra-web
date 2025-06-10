64cb36adaec91e9a1935110f5ea0833c13cd0840
JeeP12 <encabojuan@gmail.com>
2025-06-10 15:30:22 -0300

    cambio

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,19 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://l2terra.online:8080/' 
 };
+/*
+<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+
+  # Si no es un archivo o carpeta, redirige al index.html
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule ^ index.html [L]
+</IfModule>
+
+
+
+
+*/
