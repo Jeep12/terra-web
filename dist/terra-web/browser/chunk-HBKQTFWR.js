31e65a5c478234de602e0c1a680d5101af54e6ae
JeeP12 <encabojuan@gmail.com>
2025-06-10 15:55:39 -0300

    asd

---
+++ dist/terra-web/browser/chunk-HBKQTFWR.js
@@ -0,0 +1,1 @@
+var n={production:!1,apiUrl:"http://l2terra.online/",apiLocal:""};export{n as a};
--- dist/terra-web/browser/chunk-K5KFXASE.js
+++
@@ -1,1 +0,0 @@
-var n={production:!1,apiUrl:"http://l2terra.online:8080/",apiLocal:""};export{n as a};
