f99828d0e92bf3eee54a884b2ed88443250d113f
JeeP12 <encabojuan@gmail.com>
2025-07-09 01:12:17 -0300

    produccion

--- src/app/components/public/about/about.component.ts
+++ src/app/components/public/about/about.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { DiscordWidgetComponent } from "../discord-widget/discord-widget.component";
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule],
+  imports: [CommonModule, DiscordWidgetComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
